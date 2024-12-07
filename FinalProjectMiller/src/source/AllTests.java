7cfff30d6979df8abb2c0c48335e3e4fac763ecc
Brad Miller <143141311+bradleym628@users.noreply.github.com>
2024-12-06 21:27:15 -0600

    Delete FinalProjectMiller directory

--- FinalProjectMiller/src/source/AllTests.java
+++
@@ -1,9 +0,0 @@
-package source;
-
-import org.junit.platform.suite.api.SelectClasses;
-import org.junit.platform.suite.api.Suite;
-
-@Suite
-@SelectClasses({RepairTaskTest.class, CarTest.class, RepairManagerTest.class})
-public class AllTests {
-}
